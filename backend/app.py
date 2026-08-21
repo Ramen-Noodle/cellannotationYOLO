1edd92aa7db5514467323ec9a84da61708a0a291
Ramen-Noodle <clee9g0@gmail.com>
2026-08-21 14:43:31 -0400

    Changed back port number

--- backend/app.py
+++ backend/app.py
@@ -2399,4 +2399,4 @@ if __name__ == '__main__':
     print('starting application')
     # Schema is managed by Flask-Migrate now. Run `flask db upgrade` before
     # starting the app to create/update tables instead of db.create_all().
-    app.run(host='0.0.0.0', port=5002, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
